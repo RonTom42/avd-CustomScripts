4d8cae3a0f74c45aaa3df04ad933543d445d7ab1
RonTom42 <ronny.talberg@gmail.com>
2023-06-10 13:40:15 +0200

    Might work (#1)

--- HciAvdCustomScript.ps1
+++ HciAvdCustomScript.ps1
@@ -6,16 +6,36 @@ param
     [Parameter(Mandatory = $true)]
     [string]$RegistrationInfoToken,
 
-    [Parameter(mandatory = $false)] 
+    [Parameter(mandatory = $false)]
     [switch]$EnableVerboseMsiLogging
+
+    [Parameter(mandatory = $true)]
+    [switch]$DomainJoinDomain
+
+    [Parameter(mandatory = $true)]
+    [switch]$DomainJoinUser
+
+    [Parameter(mandatory = $true)]
+    [switch]$DomainJoinPassword
+
+    [Parameter(mandatory = $true)]
+    [switch]$OUPath
 )
 
-# unzip dsc zip
-$dsc_dir = (Get-Item .).FullName
-$dsc_ps_path = [IO.Path]::Combine($dsc_dir, 'Configuration.ps1')
+# Set startupType of Azure Connected Machine agent to auto (not auto delyed start)
+# for faster update in AzureRM
+sc.exe config himds start= auto
 
+# Download and unzip Microsoft's dsc zip
+$dsc_dir = (Get-Item .).FullName
+# $dsc_ps_path = [IO.Path]::Combine($dsc_dir, 'Configuration.ps1')
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Azure/RDS-Templates/master/ARM-wvd-templates/DSC/Configuration.zip' -outfile $dsc_dir\Configuration.zip -UseBasicParsing
 Get-ChildItem $dsc_dir\Configuration.zip | Expand-Archive -DestinationPath $dsc_dir
 
-# pass parameters to configuration.ps1
+# Install RDS Agent and register
 & $dsc_dir\Script-SetupSessionHost.ps1 -HostPoolName $HostPoolName -RegistrationInfoToken $RegistrationInfoToken -EnableVerboseMsiLogging:$EnableVerboseMsiLogging 
+
+# Add computer to domain
+$DomainJoinCredential = New-object –TypeName System.Management.Automation.PSCredential -ArgumentList ("$($DomainJoinDomain)\$($DomainJoinUser)", ($DomainJoinPassword | ConvertTo-SecureString –asPlainText –Force)) 
+Add-Computer -DomainName $DomainJoinDomain -OUPath $OUPath -Credential $DomainJoinCredential -Force
+ 
