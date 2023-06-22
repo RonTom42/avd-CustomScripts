20fe2999a57a634ff8bca6c62cb082394d38d07a
RonTom42 <ronny.talberg@gmail.com>
2023-06-22 14:00:39 +0200

    Working script
    * Testing
    
    * Update HciAvdCustomScript.ps1
    
    * Update HciAvdCustomScript.ps1
    
    * Update HciAvdCustomScript.ps1
    
    * test
    
    * test
    
    * no_domain_join
    
    * Update HciAvdCustomScript.ps1
    
    * Update HciAvdCustomScript.ps1
    
    * Update HciAvdCustomScript.ps1
    
    * Update HciAvdCustomScript.ps1
    
    * Update HciAvdCustomScript.ps1
    
    * Update HciAvdCustomScript.ps1
    
    * Update HciAvdCustomScript.ps1

--- HciAvdCustomScript.ps1
+++ HciAvdCustomScript.ps1
@@ -7,35 +7,22 @@ param
     [string]$RegistrationInfoToken,
 
     [Parameter(mandatory = $false)]
-    [switch]$EnableVerboseMsiLogging,
-
-    [Parameter(mandatory = $true)]
-    [switch]$DomainJoinDomain,
-
-    [Parameter(mandatory = $true)]
-    [switch]$DomainJoinUser,
-
-    [Parameter(mandatory = $true)]
-    [switch]$DomainJoinPassword,
-
-    [Parameter(mandatory = $true)]
-    [switch]$OUPath
+    [switch]$EnableVerboseMsiLogging
 )
 
 # Set startupType of Azure Connected Machine agent to auto (not auto delyed start)
 # for faster update in AzureRM
 sc.exe config himds start= auto
 
+# Disable and stop Windows Update Service
+sc.exe config wuauserv start=disabled
+sc.exe stop wuauserv
+
 # Download and unzip Microsoft's dsc zip
 $dsc_dir = (Get-Item .).FullName
-# $dsc_ps_path = [IO.Path]::Combine($dsc_dir, 'Configuration.ps1')
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Azure/RDS-Templates/master/ARM-wvd-templates/DSC/Configuration.zip' -outfile $dsc_dir\Configuration.zip -UseBasicParsing
 Get-ChildItem $dsc_dir\Configuration.zip | Expand-Archive -DestinationPath $dsc_dir
 
 # Install RDS Agent and register
-& $dsc_dir\Script-SetupSessionHost.ps1 -HostPoolName $HostPoolName -RegistrationInfoToken $RegistrationInfoToken -EnableVerboseMsiLogging:$EnableVerboseMsiLogging 
-
-# Add computer to domain
-$DomainJoinCredential = New-object –TypeName System.Management.Automation.PSCredential -ArgumentList ("$($DomainJoinDomain)\$($DomainJoinUser)", ($DomainJoinPassword | ConvertTo-SecureString –asPlainText –Force)) 
-Add-Computer -DomainName $DomainJoinDomain -OUPath $OUPath -Credential $DomainJoinCredential -Reboot -Force
- 
+write-output "Installing RDS agent..."
+& $dsc_dir\Script-SetupSessionHost.ps1 -HostPoolName $HostPoolName -RegistrationInfoToken $RegistrationInfoToken -EnableVerboseMsiLogging:$true #$EnableVerboseMsiLogging 
